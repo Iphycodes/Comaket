9073c877186c54c82124b0299cd5d2c70b7d990f
dapperwalze <dapperwalze@Walzes-MacBook-Pro.local>
2024-01-15 20:59:43 +0100

    feat: implement settings auth screens

---
+++ src/components/giro-debit/settings/account-setting/index.tsx
@@ -0,0 +1,71 @@
+'use client';
+import React from 'react';
+import { Button, Card, Col, Form, Row, Switch, TimePicker } from 'antd';
+
+type AccountSettingProps = {
+  mobileResponsive?: boolean;
+  theme?: string;
+  handleUpdateAccountSetting: (payload: Record<string, any>) => void;
+  isAccountSettingLoading: boolean;
+};
+
+export const AccountSetting = (props: AccountSettingProps) => {
+  const { handleUpdateAccountSetting, isAccountSettingLoading } = props;
+  const [form] = Form.useForm();
+
+  const onFinish = (values: Record<string, any>) => {
+    handleUpdateAccountSetting(values);
+  };
+
+  return (
+    <section className=" w-4/5 mt-10 mx-auto">
+      <Card className="shadow-sm hover:border shadow-gray-300">
+        <Form
+          form={form}
+          layout="vertical"
+          requiredMark={false}
+          onFinish={(value) => {
+            onFinish(value);
+          }}
+          name="account-setting-form"
+          className="mt-5 account-setting-form"
+        >
+          <Row gutter={[16, 16]} className="flex items center justify-center">
+            <Col md={14} xs={24}>
+              <Form.Item
+                name="reportDeliveryTime"
+                rules={[{ required: true, message: 'Select a time' }]}
+                label={<span>Report Delivery Time</span>}
+              >
+                <TimePicker className="w-full h-14" format={'HH:mm:ss'} />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row gutter={[16, 16]} className="flex items center justify-center">
+            <Col md={14} xs={24}>
+              <Form.Item
+                name="dailyTransactionReport"
+                valuePropName="checked"
+                label={<span>Dailt Transaction Report</span>}
+              >
+                <Switch size={'default'} />
+              </Form.Item>
+            </Col>
+          </Row>
+          <div className="w-full flex items-center my-3">
+            <Button
+              className="opacity-100 hover:opacity-70 mt-1.5 bg-blue text-white h-12 rounded-lg px-10 mx-auto"
+              type="primary"
+              disabled={isAccountSettingLoading}
+              block={false}
+              loading={isAccountSettingLoading}
+              htmlType="submit"
+            >
+              Update
+            </Button>
+          </div>
+        </Form>
+      </Card>
+    </section>
+  );
+};
---
+++ src/components/giro-debit/settings/api-keys-&-webhook-url/index.tsx
@@ -0,0 +1,105 @@
+'use client';
+import React from 'react';
+import { Button, Card, Col, Form, Input, Row, Spin, Tooltip } from 'antd';
+import { CopyIcon } from '@grc/_shared/assets/svgs';
+
+type ApiKeysAndWebhooksUrlProps = {
+  mobileResponsive?: boolean;
+  theme?: string;
+  handleUpdateApiKeysAndWebhooksUrl: (payload: Record<string, any>) => void;
+  isUpdatingApiKeysAndWebhooksUrl: boolean;
+  isSecretKeyLoading: boolean;
+};
+
+export const ApiKeysAndWebhooksUrl = (props: ApiKeysAndWebhooksUrlProps) => {
+  const { handleUpdateApiKeysAndWebhooksUrl, isUpdatingApiKeysAndWebhooksUrl, isSecretKeyLoading } =
+    props;
+  const [form] = Form.useForm();
+
+  const onFinish = (values: Record<string, any>) => {
+    handleUpdateApiKeysAndWebhooksUrl(values);
+  };
+
+  return (
+    <section className=" w-4/5 mt-10 mx-auto">
+      <Card className="shadow-sm hover:border shadow-gray-300">
+        <Form
+          form={form}
+          layout="vertical"
+          requiredMark={false}
+          onFinish={(value) => {
+            onFinish(value);
+          }}
+          name="update-api-key-webhook-form"
+          className="mt-5 update-api-key-webhook-form"
+        >
+          <Row gutter={[16, 16]} className="flex items center justify-center">
+            <Col md={14} xs={24}>
+              <Form.Item name="pubKey" label={<span>Live API Public Key</span>}>
+                <Input placeholder="" className="h-14" disabled />
+              </Form.Item>
+            </Col>
+            <Col className="flex items-center justify-center text-xs">
+              <div className={'flex items-center justify-center'}>
+                <CopyIcon className=" scale-75" />
+                <span>Copy</span>
+              </div>
+            </Col>
+          </Row>
+          <Row gutter={[16, 16]} className="flex items center justify-center">
+            <Col md={14} xs={24}>
+              <Form.Item name="secKey" label={<span>Live API Secret Key</span>}>
+                <Input
+                  placeholder=""
+                  className="h-14"
+                  disabled
+                  suffix={
+                    <Tooltip title="get secret key">
+                      {isSecretKeyLoading == true ? (
+                        <Spin size="small" />
+                      ) : (
+                        <i className="ri-eye-2-line cursor-pointer" />
+                      )}
+                    </Tooltip>
+                  }
+                />
+              </Form.Item>
+            </Col>
+            <Col className="flex items-center justify-center text-xs">
+              <div className={'flex items-center justify-center'}>
+                <CopyIcon className=" scale-75" />
+                <span>Copy</span>
+              </div>
+            </Col>
+          </Row>
+
+          <Row gutter={[16, 16]} className="flex items center justify-center">
+            <Col md={14} xs={24}>
+              <Form.Item
+                name="webhooks"
+                rules={[{ required: true, message: 'Enter a webhook url' }]}
+                label={<span>Webhook URL</span>}
+              >
+                <Input placeholder="" className="w-[95%] h-14" />
+              </Form.Item>
+            </Col>
+            <Col md={2} xs={0}></Col>
+          </Row>
+
+          <div className="w-full flex items-center my-3">
+            <Button
+              className="opacity-100 hover:opacity-70 mt-1.5 bg-blue text-white h-12 rounded-lg px-10 mx-auto"
+              type="primary"
+              disabled={isUpdatingApiKeysAndWebhooksUrl}
+              block={false}
+              loading={isUpdatingApiKeysAndWebhooksUrl}
+              htmlType="submit"
+            >
+              Update
+            </Button>
+          </div>
+        </Form>
+      </Card>
+    </section>
+  );
+};
