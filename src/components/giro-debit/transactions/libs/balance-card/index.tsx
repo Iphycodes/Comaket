1b9cc714d5d86b32a8db7bcf9c87ebca585ad73d
iphycodes <ifeanyiemmanuel585@gmail.com>
2024-01-18 05:52:00 -0800

    feat: remove the virtual account on transaction since we have a new global virtual account

--- src/components/giro-debit/transactions/libs/balance-card/index.tsx
+++ src/components/giro-debit/transactions/libs/balance-card/index.tsx
@@ -1,26 +1,28 @@
 import { Card, Space } from 'antd';
 import { Wallet as WalletIcon } from '@grc/_shared/assets/svgs';
-import SelectVirtualAcct from '@grc/components/giro-debit/dashboard/libs/select-virtual-acct';
 
 const BalanceCard = () => {
   return (
-    <Card className="flex flex-col border gap-5 shadow-sm shadow-gray-300 w-full">
-      <div>
-        <SelectVirtualAcct
+    <Card className="border shadow-sm shadow-gray-200 w-full">
+      <div className="flex flex-col gap-5">
+        {/* <SelectVirtualAcct
           isLoadingAccounts={false}
           vAccount={{} as any}
           accounts={[{ accountName: 'john doe', accountNumber: '00000', bankName: 'demo' } as any]}
           setVAccount={() => {}}
           className="font-bold"
-        />
-      </div>
-      <div className="flex flex-col gap-2">
-        <Space size={5}>
-          <WalletIcon />
-          <span>Giro Balance :</span>
-        </Space>
-        <div className="text-4xl font-bold">&#x20A6;25,000.00</div>
-        <div className="font-thin">Total Balance from all accounts</div>
+        /> */}
+        <div className="border p-3 px-2 text-blue font-semibold border-blue bg-cyan-50 rounded-lg shadow-sm">
+          Ifeanyi Emmanuel | 0065453363 | Demo
+        </div>
+        <div className="flex flex-col gap-2">
+          <Space size={5}>
+            <WalletIcon />
+            <span>Account Balance :</span>
+          </Space>
+          <div className="text-4xl font-bold">&#x20A6;25,000.00</div>
+          <div className="font-thin">Total Balance from all accounts</div>
+        </div>
       </div>
     </Card>
   );
