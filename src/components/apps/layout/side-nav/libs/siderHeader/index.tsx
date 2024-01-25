d957ec86a578567cefd25cac0120215a6de41e9f
dapperwalze <dapperwalze@Walzes-MacBook-Pro.local>
2024-01-25 18:52:21 +0100

    fix: fix logo for dark theme

--- src/components/apps/layout/side-nav/libs/siderHeader/index.tsx
+++ src/components/apps/layout/side-nav/libs/siderHeader/index.tsx
@@ -2,6 +2,7 @@
 import React, { Dispatch, SetStateAction } from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import { useTheme } from 'next-themes';
 
 export interface AppSettingsSiderHeaderProps {
   collapsed: boolean;
@@ -11,6 +12,7 @@ export interface AppSettingsSiderHeaderProps {
 export const AppSettingsSiderHeader = (props: AppSettingsSiderHeaderProps) => {
   const { collapsed } = props;
   const { push } = useRouter();
+  const { theme } = useTheme();
 
   return (
     <div className="flex flex-col justify-start items-start gap-10 py-2 mt-2 px-5">
@@ -18,7 +20,7 @@ export const AppSettingsSiderHeader = (props: AppSettingsSiderHeaderProps) => {
         <span className="cursor-pointer" onClick={() => push('/apps')}>
           <Image
             priority
-            src={'/assets/svgs/giro-logo.svg'}
+            src={`/assets/svgs/${theme === 'light' ? 'giro-logo' : 'giro-logo-white'}.svg`}
             alt="giro-logo"
             width={140}
             height={60}
