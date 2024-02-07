e6d4ae8ddec5a879b0f467d4e1e2b882c2e4c3a9
dapperwalze <dapperwalze@Walzes-MacBook-Pro.local>
2024-02-07 16:22:05 +0100

    fix: fix build errors

--- next.config.js
+++ next.config.js
@@ -34,6 +34,10 @@ module.exports = (phase) => {
     env,
     reactStrictMode: true,
     compiler: { styledComponents: true },
+    runtime: 'experimental-edge',
+    unstable_allowDynamic: [
+      '**/node_modules/lodash/**',
+    ],
     /* config options here */
     webpack: (config) => {
       config.module.rules.push({
